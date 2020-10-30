d0d90cca44bb9edfadf66fb170afd77fe2179d6e
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-30 08:56:34 +0000

    global variable no more

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ require './lib/game'
 
 class Battle < Sinatra::Base
   #enable :sessions
+  ['/play', "/attack", "/gameover"].each do |path|
+    before path do
+      @player1 = Game.instance_of_game.player1.name
+      @player2 = Game.instance_of_game.player2.name
+    end
+end
 
   get '/test'do
     "Testing infrastructure working"
@@ -15,26 +21,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:player1]), Player.new(params[:player2]))
+    Game.set_a_game(Player.new(params[:player1]), Player.new(params[:player2]))
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $game.player1.name
-    @player2 = $game.player2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $game.player1.name
-    @player2 = $game.player2.name
-    $game.attack
-    $game.switch
-    $game.player1.hp == 0 || $game.player2.hp == 0 ? redirect('/gameover') : erb(:attack)
+    Game.instance_of_game.attack
+    Game.instance_of_game.switch
+    Game.instance_of_game.player1.hp == 0 || Game.instance_of_game.player2.hp == 0 ? redirect('/gameover') : erb(:attack)
   end
   get '/gameover' do
-    @player1 = $game.player1.name
-    @player2 = $game.player2.name
     erb(:gameover)
   end
   run! if app_file == $0
--- lib/game.rb
+++ lib/game.rb
@@ -13,8 +13,13 @@ require_relative  'player'
 
    def switch
     @current_turn = !@current_turn
-
+   end
+   
+   def self.set_a_game(player1_instance, player2_instance)
+      @current_game = Game.new(player1_instance, player2_instance)
    end
 
-
+   def self.instance_of_game
+     @current_game
+   end
  end
